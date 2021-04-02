8e5bf335f2dc870e8c42fc0d013097a49a22c93e
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-02 10:51:20 -0700

    Create FileUtil.h

---
+++ FileUtil.h
@@ -0,0 +1,26 @@
+// Copyright Eric Chauvin 2021.
+
+
+
+#ifndef ECFileUtil_H
+#define ECFileUtil_H
+
+
+
+#include "BasicTypes.h"
+
+
+
+class FileUtil
+  {
+  private:
+
+  public:
+  static void uPrintf( const char* toWrite );
+
+  };
+
+
+#endif
+
+
