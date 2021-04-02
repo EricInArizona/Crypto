3108e1cfb1754aadd15c97599361a832bec65306
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-02 10:53:56 -0700

    Create FileUtil.cpp

---
+++ FileUtil.cpp
@@ -0,0 +1,21 @@
+// Copyright Eric Chauvin 2021.
+
+
+
+#include "FileUtil.h"
+#include <stdio.h>
+
+
+// extern "C" {
+
+
+void FileUtil::uPrintf( const char* toWrite )
+{
+// printf( "Testing.\n" );
+printf( "%s", toWrite );
+}
+
+
+
+
+
