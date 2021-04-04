01c4702b94f708590e5b55b34c0a9433ff29ae8a
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-04 15:02:08 -0700

    Update BuildProj.bat

--- BuildProj.bat
+++ BuildProj.bat
@@ -2,9 +2,12 @@
 
 rem 1> is standard out and 2> is standard error.
 
-cd \Eric\Main\Bitcoin\Cpp
+cd \Eric\Main\Crypto
 
 rem -Weverything
 rem Enable all diagnostics.
 
-C:\LLVM\bin\clang++ main.cpp FileUtil.cpp MainApp.cpp -o main.exe -Wall -O1 2> Build.log
+rem Command line reference:
+rem https://clang.llvm.org/docs/ClangCommandLineReference.html
+
+C:\LLVM\bin\clang++ @CommandLine.txt -o main.exe -Wall -O1 2> Build.log
