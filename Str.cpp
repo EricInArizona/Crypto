36a928cca9102eb2aef19da1fa6f3d60f92707b4
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-05 13:21:49 -0700

    Create Str.cpp

---
+++ Str.cpp
@@ -0,0 +1,53 @@
+// Copyright Eric Chauvin 2021.
+
+
+
+#include "Str.h"
+// #include "FileUtil.h"
+
+
+
+Str::Str( const char* pStr )
+{
+const char* sizePoint = pStr;
+
+arraySize = 0;
+for( int count = 0; count < 10000; count++ )
+  {
+  char c = *sizePoint;
+  if( c == 0 )
+    break;
+
+  sizePoint++;
+  arraySize++;
+  }
+
+cArray = new Uint16[arraySize];
+for( int count = 0; count < arraySize; count++ )
+  {
+  char c = *pStr;
+  cArray[count] = c;
+  pStr++;
+  }
+
+// FileUtil::uPrintf( "Str constructor called. " );
+// FileUtil::uPrintfD( arraySize );
+// FileUtil::uPrintf( "\n" );
+}
+
+
+
+Str::~Str( void )
+{
+delete[] cArray;
+}
+
+
+Uint16 Str::charAt( Uint32 where )
+{
+if( where >= arraySize )
+  throw "In charAt() index out of bounds.";
+
+return cArray[where];
+}
+
