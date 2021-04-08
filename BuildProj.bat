432f81706332bdc698bda917a2a5e5272f401e02
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-08 05:45:52 -0700

    Update BuildProj.bat

--- BuildProj.bat
+++ BuildProj.bat
@@ -12,5 +12,12 @@ rem https://clang.llvm.org/docs/ClangCommandLineReference.html
 
 rem @CommandLine.txt
 rem Use -v to see how it gets called.
+rem Using -v shows that Clang is using
+rem Visual Studio tools:
+rem \Community\\VC\\Tools\\MSVC\
+rem and \\Windows Kits\\10\\include\\
+rem Visual C++ C Runtime (CRT) 
+rem Universal C Runtime
+
 rem C:\LLVM\bin\clang++ main.cpp MainApp.cpp Base10Number.cpp FileUtil.cpp Base10Number.cpp -o main.exe -Wall -O1 2> Build.log
 C:\LLVM\bin\clang++ *.cpp -o main.exe -Wall -O1 2> Build.log
