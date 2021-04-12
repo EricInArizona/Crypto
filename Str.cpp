ecbb74c232833d53e4cb55b653990911ce6c7d27
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-12 12:57:19 -0700

    Update Str.cpp

--- Str.cpp
+++ Str.cpp
@@ -3,8 +3,7 @@
 
 
 #include "Str.h"
-// #include "FileUtil.h"
-
+#include "StIO.h"
 
 
 Str::Str( const char* pStr )
@@ -22,17 +21,28 @@ for( Uint32 count = 0; count < 10000; count++ )
   arraySize++;
   }
 
-cArray = new Uint16[arraySize];
+cArray = new char[arraySize];
 for( Uint32 count = 0; count < arraySize; count++ )
   {
   char c = *pStr;
   cArray[count] = c;
   pStr++;
   }
+}
 
-// FileUtil::uPrintf( "Str constructor called. " );
-// FileUtil::uPrintfD( arraySize );
-// FileUtil::uPrintf( "\n" );
+
+
+// The copy constructor.
+Str::Str( const Str& obj )
+{
+arraySize = obj.arraySize;
+cArray = new char[arraySize];
+for( Uint32 count = 0; count < arraySize; count++ )
+  {
+  cArray[count] = obj.cArray[count];
+  }
+
+StIO::uPrintf( "Str copy constructor called.\n" );
 }
 
 
@@ -43,10 +53,56 @@ delete[] cArray;
 }
 
 
-Uint16 Str::uCharAt( Uint32 where )
+char Str::charAt( Uint32 where )
 {
 if( where >= arraySize )
   throw "In charAt() index out of bounds.";
 
 return cArray[where];
+}
+
+
+void Str::print()
+{
+if( arraySize == 0 )
+  return;
+
+for( Uint32 count = 0; count < arraySize; count++ )
+  StIO::putC( cArray[count] );
+
+}
+
+
+void Str::printLine()
+{
+if( arraySize == 0 )
+  {
+  StIO::putC( '\n' );
+  return;
+  }
+
+for( Uint32 count = 0; count < arraySize; count++ )
+  StIO::putC( cArray[count] );
+
+StIO::putC( '\n' );
+}
+
+
+
+Uint32 Str::charsLength( const char* pStr )
+{
+const char* sizePoint = pStr;
+
+Uint32 howMany = 0;
+for( Uint32 count = 0; count < 10000; count++ )
+  {
+  char c = *sizePoint;
+  if( c == 0 )
+    break;
+
+  sizePoint++;
+  howMany++;
+  }
+
+return howMany;
 }
