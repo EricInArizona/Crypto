adc8564737786cfe523660db0159824950f028c5
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-13 09:03:38 -0700

    Create Uint8Array.h

---
+++ Uint8Array.h
@@ -0,0 +1,54 @@
+// Copyright Eric Chauvin 2021.
+
+
+
+#ifndef ECUint8Array_H
+#define ECUint8Array_H
+
+
+
+#include "BasicTypes.h"
+#include "Str.h"
+
+
+class Uint8Array
+  {
+  private:
+  Uint32 arraySize = 0;
+  Uint32 last = 0;
+  Uint8* cArray;
+
+  void increaseSize( Uint32 howMuch );
+
+  public:
+  Uint8Array( void );
+  Uint8Array( const Uint8Array& obj );
+  ~Uint8Array( void );
+  inline Uint32 getLast( void )
+    {
+    return last;
+    }
+
+  inline void clear( void )
+    {
+    last = 0;
+    }
+
+  const Uint8* getBufPoint( void )
+    {
+    return cArray;
+    }
+
+  void appendVal( const Uint8 toSet );
+  void appendArray( const Uint8* fromBuf,
+                                Uint32 howMany );
+  void appendStr( Str& in );
+  void reverse( void );
+  Uint8 valAt( Uint32 where );
+  bool isAllZero( void );
+
+
+  };
+
+
+#endif
