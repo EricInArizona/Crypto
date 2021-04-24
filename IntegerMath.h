5b662680b442ea02b7c4806089f2b1e446084c58
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-24 11:46:25 -0700

    Create IntegerMath.h

---
+++ IntegerMath.h
@@ -0,0 +1,86 @@
+// Copyright Eric Chauvin, 2021.
+
+
+
+#ifndef ECIntegerMath_H
+#define ECIntegerMath_H
+
+
+#include "BasicTypes.h"
+#include "Integer.h"
+#include "SPrimes.h"
+#include "Str.h"
+
+
+class IntegerMath
+  {
+  private:
+  // Signed digits for use in subtraction.
+  Int64 signedD[Integer::digitArraySize] =
+                                       { 1,2,3 };
+
+  // Scratch pad, just like you would do on paper.
+  Uint64 M[Integer::digitArraySize]
+          [Integer::digitArraySize] =
+                                      { {1,2,3},
+                                      {1,2,3} };
+  Uint64 scratch[Integer::digitArraySize] =
+                                      { 1,2,3 };
+
+  // ModularReduction ModReduction;
+
+  void setMultiplySign( Integer& result,
+                        Integer& toMul );
+
+  void searchSqrtXPart( Uint32 testIndex,
+                        Integer& fromSqr,
+                        Integer& sqrRoot );
+
+
+  public:
+  SPrimes sPrimes;
+
+  IntegerMath( void );
+  IntegerMath( const IntegerMath& obj );
+  static Uint64 findULSqrRoot( Uint64 toMatch );
+  Uint64 isDivisibleBySmallPrime(
+                              Integer& toTest );
+  void subtractULong( Integer& result,
+                                  Uint64 toSub );
+  void add( Integer& result, Integer& toAdd );
+  void subtract( Integer& result,
+                               Integer& toSub );
+  void subtractPositive( Integer& result,
+                               Integer& toSub );
+  void multiplyUInt( Integer& result,
+                                Uint64 toMul );
+
+  Uint32 multiplyUIntFromCopy( Integer& result,
+                             Integer& fromCopy,
+                             Uint64 toMul );
+  void multiplyULong( Integer& result,
+                                  Uint64 toMul );
+  void multiply( Integer& result,
+                               Integer& toMul );
+  void setFromStr( Integer& result, Str& in );
+  Str toString10( Integer& from );
+  void square( Integer& toSquare );
+  void multiplyTop( Integer& result,
+                               Integer& toMul );
+  void multiplyTopOne( Integer& result,
+                               Integer& toMul );
+  bool squareRoot( Integer& fromSqr,
+                            Integer& sqrRoot );
+  bool isFermatPrime( Integer& toTest,
+                                 Uint32 howMany );
+  bool isFermatPrimeForOneValue(
+                   Integer& toTest, Uint64 base );
+
+
+  };
+
+
+
+
+
+#endif
