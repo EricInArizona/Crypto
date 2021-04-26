688988dfcc303abbfd7f369601154bd0fae059e1
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-04-25 17:01:00 -0700

    Update MainApp.h

--- MainApp.h
+++ MainApp.h
@@ -6,9 +6,6 @@
 #define ECMainApp_H
 
 
-// An old saying: "Make it work first, then make
-// it work fast".
-
 
 #include "BasicTypes.h"
 
@@ -23,7 +20,7 @@ class MainApp
   public:
   inline static const char* getVersionStr( void )
     {
-    return "4/12/2021";
+    return "4/25/2021";
     }
 
   Int32 mainLoop( Int32 argc, char* argv[] );
