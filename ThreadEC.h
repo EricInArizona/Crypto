973e816d0606413be02b1b49d71d81e29a7fc293
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-05-22 10:12:41 -0600

    Create ThreadEC.h

---
+++ ThreadEC.h
@@ -0,0 +1,24 @@
+// Copyright Eric Chauvin 2021.
+
+
+
+#ifndef ThreadEC_H
+#define ThreadEC_H
+
+
+
+#include "BasicTypes.h"
+
+
+class ThreadEC
+  {
+  private:
+
+  public:
+  static void sleep( Uint32 milli );
+
+  };
+
+
+#endif
+
