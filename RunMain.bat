00731e20784d3085c346a027181e792a7871a149
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-06-08 16:55:59 -0600

    Update RunMain.bat

--- RunMain.bat
+++ RunMain.bat
@@ -6,3 +6,11 @@ cd \Eric\Main\Crypto
 del ExeOut.txt
 rem \Eric\Main\Crypto\main.exe > ExeOut.txt
 \Eric\Main\Crypto\main.exe
+
+echo The error level is:
+echo %ERRORLEVEL% 
+
+rem IF %ERRORLEVEL% NEQ 0 ( 
+rem   DO_Something 
+rem )
+
