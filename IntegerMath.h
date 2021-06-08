d4dd04c3e184aa926c802284612478fc71c135d1
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-06-08 13:22:42 -0600

    Update IntegerMath.h

--- IntegerMath.h
+++ IntegerMath.h
@@ -10,6 +10,7 @@
 #include "Integer.h"
 #include "SPrimes.h"
 #include "Str.h"
+#include "FileIO.h"
 
 
 class IntegerMath
@@ -17,7 +18,7 @@ class IntegerMath
   private:
   // Signed digits for use in subtraction.
   Int64 signedD[Integer::digitArraySize] =
-                                       { 1,2,3 };
+                                     { 1,2,3 };
 
   // Scratch pad, just like you would do on paper.
   Uint64 M[Integer::digitArraySize]
@@ -30,37 +31,48 @@ class IntegerMath
   void setMultiplySign( Integer& result,
                         Integer& toMul );
 
+/*
   void searchSqrtXPart( Uint32 testIndex,
                         Integer& fromSqr,
                         Integer& sqrRoot );
 
+*/
 
   public:
   IntegerMath( void );
-  IntegerMath( const IntegerMath& obj );
+  IntegerMath( const IntegerMath& in );
   static Uint64 findULSqrRoot( Uint64 toMatch );
   Uint64 isDivisibleBySmallPrime(
                               Integer& toTest,
                               SPrimes& sPrimes );
+
   void subtractULong( Integer& result,
                                   Uint64 toSub );
+/*
   void add( Integer& result, Integer& toAdd );
   void subtract( Integer& result,
                                Integer& toSub );
   void subtractPositive( Integer& result,
                                Integer& toSub );
+*/
+
   void multiplyUInt( Integer& result,
                                 Uint64 toMul );
-
   Uint32 multiplyUIntFromCopy( Integer& result,
-                             Integer& fromCopy,
+                             Integer& from,
                              Uint64 toMul );
   void multiplyULong( Integer& result,
-                                  Uint64 toMul );
+                           Uint64 toMul );
   void multiply( Integer& result,
                                Integer& toMul );
+
+/*
   void setFromStr( Integer& result, Str& in );
+*/
+
   Str toString10( Integer& from );
+
+/*
   void square( Integer& toSquare );
   void multiplyTop( Integer& result,
                                Integer& toMul );
@@ -68,6 +80,8 @@ class IntegerMath
                                Integer& toMul );
   bool squareRoot( Integer& fromSqr,
                             Integer& sqrRoot );
+*/
+
 
   };
 
