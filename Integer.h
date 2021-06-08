9c7b67c73f9aaef763124780b246f02176235b09
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-06-08 13:23:27 -0600

    Update Integer.h

--- Integer.h
+++ Integer.h
@@ -30,24 +30,27 @@ class Integer
   // stack pointer.
 
   // It is Uint64 to hold multiplied values.
-  // Uint64 D[digitArraySize] = { };
   Uint64 D[digitArraySize] = { 0,1,2 };
   Uint64 scratch[digitArraySize] =  { 0,1,2 };
 
+/*
   void setOneDValueFromChar( Uint64 toSet,
                 Uint32 position, Uint32 offset );
   char getOneCharFromDValue(
                 Uint32 position, Uint32 offset );
   void setOneDValueFromByte( Uint64 toSet,
-                  Uint32 setIndex, Uint32 offset );
+                             Uint32 atIndex,
+                             Uint32 offset );
   char getOneByteFromDValue(
                   Uint32 atIndex, Uint32 offset );
 
+*/
 
   public:
   Integer( void );
   Integer( const Integer& obj );
   // ~Integer();
+
   inline void setToZero( void )
     {
     isNegative = false;
@@ -55,12 +58,14 @@ class Integer
     D[0] = 0;
     }
 
+/*
   inline void setToOne( void )
     {
     isNegative = false;
     index = 0;
     D[0] = 1;
     }
+*/
 
   inline bool isZero( void )
     {
@@ -98,24 +103,31 @@ class Integer
 
   inline void setIndex( Uint32 where )
     {
+    if( where >= digitArraySize )
+      throw "setIndex() index out of bounds.";
+
     index = where;
     }
 
+
   inline Uint64 getD( Uint32 where )
     {
-    // if( where >= digitArraySize )
+    if( where >= digitArraySize )
+      throw "getD() index out of bounds.";
 
     return D[where];
     }
 
   inline void setD( Uint32 where, Uint32 toSet )
     {
-    // if( where >= digitArraySize )
+    if( where >= digitArraySize )
+      throw "setD() index out of bounds.";
 
     D[where] = toSet;
     }
 
   void incrementIndex( void );
+/*
   void setToMaxValue( void );
   inline void setFromUInt( Uint32 toSet )
     {
@@ -123,21 +135,36 @@ class Integer
     D[0] = toSet;
     index = 0;
     }
+*/
 
   void setFromULong( Uint64 toSet );
   void copy( const Integer& copyFrom );
+
+/*
   void copyFromP( const Integer* copyFrom );
+*/
+
   void copyUpTo( const Integer& copyFrom,
                                  Uint32 where );
+
+/*
   bool isEqualToULong( Uint64 toTest );
+*/
+
   bool isEqual( Integer& x );
   bool isULong( void );
   Uint64 getAsULong( void );
   bool paramIsGreater( Integer& x );
+
+/*
   bool paramIsGreaterOrEq( Integer& x );
   void increment( void );
+*/
+
   void addULong( Uint64 toAdd );
   void add( Integer& toAdd );
+
+/*
   void square0( void );
   void square1( void );
   void square2( void );
@@ -145,7 +172,11 @@ class Integer
   void shiftRight( Uint32 shiftBy );
   void setDigitAndClear( Uint32 where,
                                   Uint64 toSet );
+*/
+
   bool makeRandomOdd( Uint32 setToIndex );
+
+/*
   bool setFromAsciiStr( Str& in );
   Str getAsciiStr( void );
   void setFromBigEndianByteArray(
@@ -155,11 +186,13 @@ class Integer
   static Uint64 mod64FromTwoULongs( Uint64 P1,
                                  Uint64 P0,
                                  Uint64 divisor );
+*/
+
   Uint64 getMod32( Uint64 divisor );
+
+/*
   Uint64 getMod64( Uint64 divisor );
-
-
-
+*/
   };
 
 
