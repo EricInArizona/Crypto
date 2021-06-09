cbe27ab7f6003f079ed899cd1a06d505e0d99d96
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-06-09 06:00:37 -0600

    Update BuildProj.bat

--- BuildProj.bat
+++ BuildProj.bat
@@ -16,8 +16,24 @@ rem Using -v shows that Clang is using
 rem Visual Studio tools:
 rem \Community\\VC\\Tools\\MSVC\
 rem and \\Windows Kits\\10\\include\\
-rem Visual C++ C Runtime (CRT) 
+rem Visual C++ C Runtime (CRT)
 rem Universal C Runtime
 
+rem C:\LLVM\bin\clang++ --help
 rem C:\LLVM\bin\clang++ main.cpp MainApp.cpp Base10Number.cpp FileUtil.cpp Base10Number.cpp -o main.exe -Wall -O1 2> Build.log
-C:\LLVM\bin\clang++ *.cpp -o main.exe -Wall -O1 2> Build.log
+
+rem -MAP goes to main.map.
+C:\LLVM\bin\clang++ *.cpp -o main.exe -Wall -O1 -Wl,-MAP -Wl,-STACK:10000000 2> Build.log
+
+rem https://docs.microsoft.com/en-us/cpp/build/reference/stack-stack-allocations?view=msvc-160
+rem Tell the linker to use this max stack size.
+rem -Wl,-STACK:10000000
+rem -Wl,"-stack_size 0x100000"
+rem On Windows, clang calls link.exe.
+rem "For ARM, x86 and x64 machines, the default
+rem stack size is 1 MB."
+
+rem -Wl,<arg>
+rem Pass the comma separated arguments in
+rem <arg> to the linker
+
