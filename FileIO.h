f477edc4bfd2775aef4c9a084873d5f5fa6bdadc
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-06-22 08:35:27 -0600

    Update FileIO.h

--- FileIO.h
+++ FileIO.h
@@ -15,10 +15,10 @@ class FileIO
   {
   private:
   CharBuf cBuf;
-
+  Uint32 testForCopy = 123;
   public:
   FileIO( void );
-  FileIO( const FileIO &obj );
+  FileIO( const FileIO &in );
   ~FileIO( void );
   void readAll( const char* fileName );
   void writeAll( const char* fileName );
