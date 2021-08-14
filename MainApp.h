fc436a8a01e7b7634359c540428019e989db809b
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-08-14 14:34:39 -0700

    Update MainApp.h

--- MainApp.h
+++ MainApp.h
@@ -21,7 +21,7 @@ class MainApp
   public:
   inline static const char* getVersionStr( void )
     {
-    return "8/9/2021";
+    return "8/14/2021";
     }
 
   // Int32 mainLoop( Int32 argc, char* argv[] );
