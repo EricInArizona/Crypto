32373a8216db2d5cc2aa03a384e9a5245afaf645
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-08-17 06:28:30 -0700

    Create OneDUint64.h

---
+++ OneDUint64.h
@@ -0,0 +1,40 @@
+// Copyright Eric Chauvin, 2021.
+
+
+
+#pragma once
+
+
+
+#include "BasicTypes.h"
+#include "Integer.h"
+
+
+class OneDUint64
+  {
+  private:
+  Uint64* aR;
+  static const Uint32 last =
+                      Integer::digitArraySize;
+
+  public:
+  OneDUint64( void );
+  ~OneDUint64( void );
+  inline Uint64 getV( Uint32 where )
+    {
+    if( where >= last )
+      throw "OneDUint64 get out of bounds.";
+
+    return aR[where];
+    }
+
+  inline void setV( Uint32 where, Uint64 val )
+    {
+    if( where >= last )
+      throw "OneDUint64 set out of bounds.";
+
+    aR[where] = val;
+    }
+
+
+  };
