93e8facba5ba70230c398dda82731e051e4d5679
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-08-18 18:12:43 -0700

    Update Mod.cpp

--- Mod.cpp
+++ Mod.cpp
@@ -4,6 +4,7 @@
 
 #include "Mod.h"
 #include "Division.h"
+#include "Euclid.h"
 
 
 
@@ -53,11 +54,10 @@ exact.copy( remainder );
 
 // x^n = (x^2)^((n - 1)/2) if n is odd.
 // x^n = (x^2)^(n/2)       if n is even.
-void Mod::toPower( // FileIO& mainIO,
-                        Integer& result,
-                        Integer& exponent,
-                        Integer& modulus,
-                        IntegerMath& intMath )
+void Mod::toPower( Integer& result,
+                   Integer& exponent,
+                   Integer& modulus,
+                   IntegerMath& intMath )
 {
 if( result.isZero())
   {
@@ -105,41 +105,40 @@ if( exponent.isOne())
   return;
   }
 
-Integer xForModPower;
-Integer exponentCopy;
-Integer tempForModPower;
+Integer X;
+Integer expCopy;
+Integer temp;
 
-xForModPower.copy( result );
-exponentCopy.copy( exponent );
+X.copy( result );
+expCopy.copy( exponent );
 // Uint32 testIndex = 0;
 
 result.setToOne();
 
+Int32 howBig = 0;
+
 // For each bit.
 while( true )
   {
-  if( (exponentCopy.getD( 0 ) & 1) == 1 )
+  if( (expCopy.getD( 0 ) & 1) == 1 )
     {
-    intMath.multiply( result, xForModPower );
+    intMath.multiply( result, X );
 
-    // tempForModPower is the result of this
+    // temp is the result of this
     // reduce() operation.
-    numbSys.reduce( tempForModPower,
-                    result, modulus, intMath );
-    result.copy( tempForModPower );
+    numbSys.reduce( temp, result, modulus,
+                                      intMath );
+    result.copy( temp );
     }
 
-  exponentCopy.shiftRight( 1 ); // Divide by 2.
-  if( exponentCopy.isZero())
+  expCopy.shiftRight( 1 ); // Divide by 2.
+  if( expCopy.isZero())
     break;
 
-  // Square it.
-  // intMath.multiply( xForModPower, xForModPower );
-  intMath.square( xForModPower );
-
-  numbSys.reduce( tempForModPower,
-                xForModPower, modulus, intMath );
-  xForModPower.copy( tempForModPower );
+  // intMath.multiply( X, X );
+  intMath.square( X );
+  numbSys.reduce( temp, X, modulus, intMath );
+  X.copy( temp );
   }
 
 // When reduce() gets called it multiplies a base
@@ -149,7 +148,7 @@ while( true )
 // you can get carry bits that can make it a
 // little bigger.
 
-Int32 howBig = (Int32)result.getIndex() -
+howBig = (Int32)result.getIndex() -
                (Int32)modulus.getIndex();
 // if( howBig > 1 )
   // throw "This does happen.";
@@ -157,9 +156,8 @@ Int32 howBig = (Int32)result.getIndex() -
 if( howBig > 2 )
   throw "This never happens yet. howBig.";
 
-numbSys.reduce( tempForModPower, result,
-                            modulus, intMath );
-result.copy( tempForModPower );
+numbSys.reduce( temp, result, modulus, intMath );
+result.copy( temp );
 
 // Notice that this Divide() is done once.
 // Not a thousand or two thousand times.
@@ -168,165 +166,177 @@ makeExact( result, modulus, intMath );
 
 
 
+void Mod::verifyInBaseRange( Integer& toCheck,
+                             Integer& modulus )
+{
+if( toCheck.getIsNegative() )
+  throw "verifyInBaseRange() is negative.";
 
-/*
-Java code:
+if( modulus.paramIsGreaterOrEq( toCheck ))
+  throw "verifyInBaseRange() modulus >= toCheck";
 
-  public static void verifyInBaseRange(
-                                   long base,
-                                   long toCheck )
-                                 throws Exception
-    {
-    if( toCheck < 0 )
-      throw new Exception( "verifyInBaseRange()
-                               exception 1." );
-
-    if( toCheck >= base )
-      throw new Exception( "verifyInBaseRange()
-                                 exception 2." );
-
-    }
-
-
-  public static void verifyMoreThanZero(
-                                   long toCheck )
-                                 throws Exception
-    {
-    if( toCheck <= 0 )
-      throw new Exception(
-                      "verifyMoreThanZero()." );
-
-    }
+}
 
 
 
-  public static long add( long base, long in,
-                                     long toAdd )
-                                  throws Exception
-    {
-    verifyInBaseRange( base, toAdd );
-    verifyInBaseRange( base, in );
+void Mod::verifyMoreThanZero( Integer& toCheck )
+{
+if( toCheck.getIsNegative() )
+  throw "verifyMoreThanZero() is negative.";
 
-    long result = in + toAdd;
-    result = result % base;
-    return result;
-    }
+if( toCheck.isZero() )
+  throw "verifyMoreThanZero() is zero.";
 
-
-  public static long negate( long base, long in )
-                               throws Exception
-    {
-    // If y was 1 and base was 5.
-    // 1 + (-1) = 0;
-    // y = -1 + 5 = 4.
-    // 1 + 4 = 0 mod 5.
-    // The definition of the word 'negate':
-    // y + negate( y ) = 0.
-
-    // If I was using a table (array) I could use
-    // only symbols from within the range zero
-    // to base - 1.  But I use negative numbers
-    // and numbers that are more than or equal
-    // to base in order to do the math.
-    // Like this assignment: result = -in.
-    // I have to go outside of the set of numbers
-    // in order to find the right numbers that
-    // are inside of the set.
-
-    verifyInBaseRange( base, in );
-    long result = -in;
-    result += base;
-    return result;
-    }
+}
 
 
 
-  public static long subtract( long base, long in,
-                               long toSub )
-                               throws Exception
-    {
-    verifyInBaseRange( base, in );
-    verifyInBaseRange( base, toSub );
-    long result = in - toSub;
-    if( result < 0 )
-      result += base;
+void Mod::add( Integer& result,
+               Integer& toAdd, Integer& modulus,
+               IntegerMath& intMath )
+{
+verifyInBaseRange( result, modulus );
+verifyInBaseRange( toAdd, modulus );
 
-    result = result % base;
-    return result;
-    }
+// Integer temp;
 
+intMath.add( result, toAdd );
 
-
-  public static long multiply( long base, long in,
-                               long toMul )
-                               throws Exception
-    {
-    verifyInBaseRange( base, in );
-    verifyInBaseRange( base, toMul );
-    long result = in * toMul;
-    result = result % base;
-    return result;
-    }
+// numbSys.reduce( temp, result, modulus, intMath );
+// result.copy( temp );
+makeExact( result, modulus, intMath );
+}
 
 
+void Mod::negate( Integer& result,
+                  Integer& modulus,
+                  IntegerMath& intMath )
+{
+// If y was 1 and base was 5.
+// 1 + (-1) = 0;
+// y = -1 + 5 = 4.
+// 1 + 4 = 0 mod 5.
+// The definition of the word 'negate':
+// y + negate( y ) = 0.
 
-  public static long square( long base, long in )
-                               throws Exception
-    {
-    verifyInBaseRange( base, in );
-    long result = in * in;
-    result = result % base;
-    return result;
-    }
+verifyInBaseRange( result, modulus );
+
+// Since it was checked to see if it's in
+// the base range, it is not negative.
+
+result.setIsNegative( true );
+// Adding a positive number to a negative
+// number to make it positive.
+intMath.add( result, modulus );
+}
 
 
 
-  public static long divide( long base,
-                             long numerator,
-                             long divisor )
-                             throws Exception
-    {
-    verifyInBaseRange( base, divisor );
-    verifyInBaseRange( base, numerator );
-    verifyMoreThanZero( divisor );
+void Mod::subtract( Integer& result,
+                    Integer& toSub,
+                    Integer& modulus,
+                    IntegerMath& intMath )
+{
+verifyInBaseRange( result, modulus );
+verifyInBaseRange( toSub, modulus );
 
-    if( numerator == 0 )
-      return 0;
+intMath.subtract( result, toSub );
+if( result.getIsNegative())
+  result.add( modulus );
 
-    if( divisor == 1 )
-      {
-      // num / denom = result
-      // 1 / 1 = 1
-      // num * denom^-1 = result
-      // num * 1 = num
-      return numerator;
-      }
+verifyInBaseRange( result, modulus );
 
-    // The definition of multiplicative inverse:
-    // soAndSo * multInverse == 1.
-    // You can't have a multiplicative inverse
-    // of zero.  0 * anything = 0.  So it can't
-    // equal 1.
+// numbSys.reduce( temp, result, modulus, intMath );
+// result.copy( temp );
+// makeExact( result, modulus, intMath );
+}
 
-    // base should be a prime, so the GCD with
-    // divisor is 1.
 
-    // Get the multiplicative inverse.
-    long inv = Euclid.multInverse( base, divisor );
-    // Returns 0 if they had a GCD > 1.
-    if( inv == 0 )
-      {
-      throw new Exception(
-             "Inv == 0 in divide(). " +
-                divisor + ", " + base );
-      }
 
-    verifyInBaseRange( base, inv );
+void Mod::multiply( Integer& result,
+                    Integer& toMul,
+                    Integer& modulus,
+                    IntegerMath& intMath )
+{
+verifyInBaseRange( result, modulus );
+verifyInBaseRange( toMul, modulus );
 
-    long result = numerator * inv;
-    result = result % base;
-    return result;
-    }
+intMath.multiply( result, toMul );
 
-Java code.
-*/
+Integer temp;
+
+numbSys.reduce( temp, result, modulus, intMath );
+result.copy( temp );
+makeExact( result, modulus, intMath );
+}
+
+
+
+void Mod::square( Integer& result,
+                  Integer& modulus,
+                  IntegerMath& intMath )
+{
+verifyInBaseRange( result, modulus );
+
+intMath.square( result );
+
+Integer temp;
+
+numbSys.reduce( temp, result, modulus, intMath );
+result.copy( temp );
+makeExact( result, modulus, intMath );
+}
+
+
+
+void Mod::divide( Integer& result,
+                  Integer& numerator,
+                  Integer& divisor,
+                  Integer& modulus,
+                  IntegerMath& intMath )
+{
+verifyInBaseRange( divisor, modulus );
+verifyInBaseRange( numerator, modulus );
+verifyMoreThanZero( divisor );
+
+if( numerator.isZero())
+  {
+  result.setToZero();
+  return;
+  }
+
+if( divisor.isOne() )
+  {
+  result.copy( numerator );
+  return;
+  }
+
+// The definition of multiplicative inverse:
+// soAndSo * multInverse == 1.
+// You can't have a multiplicative inverse
+// of zero.  0 * anything = 0, so it can't
+// equal 1.
+
+// base should be a prime, so the GCD with
+// divisor is 1.
+
+Integer inverse;
+
+// Get the multiplicative inverse.
+if( !Euclid::multInverse( divisor,
+                          modulus,
+                          inverse,
+                          intMath ))
+  throw "GCD not 1 in Mod::divide().";
+
+verifyInBaseRange( result, modulus );
+
+result.copy( numerator );
+intMath.multiply( result, inverse );
+
+Integer temp;
+
+numbSys.reduce( temp, result, modulus, intMath );
+result.copy( temp );
+makeExact( result, modulus, intMath );
+}
