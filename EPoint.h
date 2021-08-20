41bab0ac4d999bc72afb18b561e32ddc51519d0f
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-08-20 16:12:08 -0700

    Update EPoint.h

--- EPoint.h
+++ EPoint.h
@@ -12,66 +12,67 @@
 // #include "FileIO.h"
 
 
-// This is for the arithmetic for points on
-// an elliptic curve.  Add, subtract, multiply
-// and divide points on a curve.  If the points
-// are on the curve, then doing these operations
-// will result in another point on the curve.
-
+// A point on an elliptic curve.
 
 class EPoint
   {
   private:
-  Int32 testForCopy = 123;
-  bool isInfinite = false;
+  bool infin = false; // Infinite
   Integer X;
   Integer Y;
 
   public:
   EPoint( void );
   EPoint( const EPoint& in );
-  EPoint( Integer& setX, Integer& setY );
-  EPoint( EPoint& in );
+  EPoint( const Integer& setX,
+          const Integer& setY );
 
   // The curve used in Bitcoin: y^2 = x^3 + 7
   static const Uint32 coefA = 0;
   static const Uint32 coefB = 7;
 
-  inline bool getIsInfinite( void )
+  inline bool getInfin( void ) const
     {
-    return isInfinite;
+    return infin;
     }
 
-  inline void setIsInfinite( bool setTo )
+  inline void setInfin( bool setTo )
     {
-    isInfinite = setTo;
+    infin = setTo;
     }
 
-  void copy( EPoint& p );
-  bool isOnCurve( Integer& modulus,
-              Mod& mod, IntegerMath& intMath );
-  bool isEqual( EPoint& in );
-  bool isConjugate( EPoint& p,
-                    Integer& modulus,
+  void copy( const EPoint& p );
+  bool isEqual( const EPoint& in ) const;
+  bool isConjugate( const EPoint& p,
+                    const Integer& modulus,
                     Mod& mod,
-                    IntegerMath& intMath );
-  void add( EPoint& p, EPoint& q,
-            Integer& modulus,
+                    IntegerMath& intMath ) const;
+  bool isOnCurve( const Integer& modulus,
+              Mod& mod, IntegerMath& intMath );
+  void add( const EPoint& p, const EPoint& q,
+            const Integer& modulus,
             Mod& mod,
             IntegerMath& intMath  );
-  void doubleP( EPoint& p,
-                Integer& modulus,
+  void doubleP( const EPoint& p,
+                const Integer& modulus,
                 Mod& mod,
                 IntegerMath& intMath );
-  void repeatDoubleP( Integer& twoPower,
-                      EPoint p,
-                      Integer& modulus,
+  void scalarMultByAdd( const Uint32 k,
+                        const Integer& modulus,
+                        Mod& mod,
+                        IntegerMath& intMath );
+
+/*
+  void twoPowerByAdd( const EPoint& p,
+                      const Uint32 twoPower,
+                      const Integer& modulus,
                       Mod& mod,
                       IntegerMath& intMath );
-  void repeatDoublePByAdd( EPoint& p,
-                         Integer& twoPower,
-                         Integer& modulus,
-                         Mod& mod,
-                         IntegerMath& intMath );
+*/
+  void twoPowerDoubleP( const Uint32 twoPower,
+                        const EPoint p,
+                        const Integer& modulus,
+                        Mod& mod,
+                        IntegerMath& intMath );
 
   };
