c837f035ff2979f2f935f8b5d242020c51c32882
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-08-20 12:58:59 -0700

    Update Division.h

--- Division.h
+++ Division.h
@@ -16,49 +16,55 @@ class Division
   private:
 
   public:
-  static bool shortDivide( Integer& toDivide,
-                    Integer& divideBy,
-                    Integer& quotient,
-                    Integer& remainder );
+  static bool shortDivide(
+            const Integer& toDivideOriginal,
+            const Integer& divideByOriginal,
+            Integer& quotient,
+            Integer& remainder );
 
   static Uint64 shortDivideRem(
-                    Integer& toDivideOriginal,
-                    Uint64 divideByU,
-                    Integer& quotient );
+                const Integer& toDivideOriginal,
+                const Uint64 divideByU,
+                Integer& quotient );
 
-  static void divide( Integer& toDivideOriginal,
-                    Integer& divideByOriginal,
+  static void divide(
+                const Integer& toDivideOriginal,
+                const Integer& divideByOriginal,
+                Integer& quotient,
+                Integer& remainder,
+                IntegerMath& intMath );
+
+  static bool longDivide1(
+                    const Integer& toDivide,
+                    const Integer& divideBy,
                     Integer& quotient,
                     Integer& remainder,
                     IntegerMath& intMath );
 
-  static bool longDivide1( Integer& toDivide,
-                    Integer& divideBy,
-                    Integer& quotient,
-                    Integer& remainder,
-                    IntegerMath& intMath );
-
-  static void testDivideBits( Uint64 maxValue,
-                       bool isTop,
-                       Uint32 testIndex,
-                       Integer& toDivide,
-                       Integer& divideBy,
-                       Integer& quotient,
+  static void testDivideBits(
+                      const Uint64 maxValue,
+                      const bool isTop,
+                      const Uint32 testIndex,
+                      const Integer& toDivide,
+                      const Integer& divideBy,
+                      Integer& quotient,
                        // Integer& remainder,
                        IntegerMath& intMath );
 
-  static bool longDivide2( Integer& toDivide,
-                         Integer& divideBy,
-                         Integer& quotient,
-                         Integer& remainder,
-                         IntegerMath& intMath );
+  static bool longDivide2(
+                      const Integer& toDivide,
+                      const Integer& divideBy,
+                      Integer& quotient,
+                      Integer& remainder,
+                      IntegerMath& intMath );
 
   static Uint32 findShiftBy( Uint64 toTest );
 
-  static void longDivide3( Integer& toDivide,
-                         Integer& divideBy,
-                         Integer& quotient,
-                         Integer& remainder,
-                         IntegerMath& intMath );
+  static void longDivide3(
+                      const Integer& toDivide,
+                      const Integer& divideBy,
+                      Integer& quotient,
+                      Integer& remainder,
+                      IntegerMath& intMath );
 
   };
