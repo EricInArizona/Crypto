a55c5477e83f97cffcd5901bef0427811aef4281
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-08-21 09:17:17 -0700

    Create Ellip.h

---
+++ Ellip.h
@@ -0,0 +1,39 @@
+// Copyright Eric Chauvin, 2021.
+
+
+
+#pragma once
+
+
+// Elliptic Curve Crypto Systems
+
+
+#include "BasicTypes.h"
+#include "Integer.h"
+#include "Mod.h"
+#include "FileIO.h"
+#include "EPoint.h"
+
+
+
+class Ellip
+  {
+  private:
+  Int32 testForCopy = 123;
+  IntegerMath intMath;
+  SPrimes sPrimes;
+  Mod mod;
+  EPoint* pArray;
+  static const Uint32 pArrayLength = 1024 * 8;
+
+  public:
+  Ellip( void );
+  Ellip( const Ellip& in );
+  ~Ellip( void );
+  void test( FileIO& mainIO );
+  void mainTest( FileIO& mainIO );
+  void makeCurvePoints(  FileIO& mainIO,
+                         const Uint32 prime );
+
+  };
+
