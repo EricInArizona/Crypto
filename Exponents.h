935af280dff1c509569e2bf80685fa0a53a63020
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-11-12 12:18:09 -0700

    Update Exponents.h

--- Exponents.h
+++ Exponents.h
@@ -1,8 +1,6 @@
 // Copyright Eric Chauvin 2021.
 
 
-// Exponents and Discrete Logarithms
-
 
 #pragma once
 
@@ -12,24 +10,39 @@
 #include "IntegerMath.h"
 #include "NumbSys.h"
 #include "Mod.h"
-#include "TwoDUint64.h"
+#include "IntBuf.h"
+
+
+// The size of this is the number of bits
+// times the size of an IntBuf.  An IntBuf takes
+// up about ProjConst::digitArraySize
+// time 4 bytes, or about 2,000 bytes each.
+// So something like 2,000 times 1,000 bytes.
 
 
 class Exponents
   {
   private:
   Int32 testForCopy = 123;
+  static const Uint32 last = 1000;
   NumbSys numbSys;
   Mod mod;
-  TwoDUint64 baseArray;
+  IntBuf* intBufAr;
+  Integer currentBase;
+  Integer currentModulus;
 
   public:
   Exponents( void );
   Exponents( const Exponents& in );
-  void toPower( Integer& result,
-                   const Integer& exponent,
+  ~Exponents( void );
+  void setupBases( const Integer& base,
                    const Integer& modulus,
-                   IntegerMath& intMath );
-
+                   IntegerMath& intMath ); // ,
+                   // FileIO& mainIO );
+  void toPower( Integer& result,
+                const Integer& exponent,
+                const Integer& modulus,
+                IntegerMath& intMath,
+                FileIO& mainIO );
 
   };
