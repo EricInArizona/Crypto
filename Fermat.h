bec7ddfc4b0b9e2b50b90fcf46fee60da7c3d5d7
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-11-13 11:36:39 -0700

    Update Fermat.h

--- Fermat.h
+++ Fermat.h
@@ -10,6 +10,7 @@
 #include "Integer.h"
 #include "IntegerMath.h"
 #include "Mod.h"
+// #include "Exponents.h"
 
 
 
@@ -23,18 +24,24 @@ class Fermat
                    Uint32 setToIndex,
                    SPrimes& sPrimes,
                    IntegerMath& intMath,
-                   Mod& mod );
+                   Mod& mod ); // ,
+                   // Exponents& exp,
+                   // FileIO& mainIO );
 
   static bool isPrime( Integer& toTest,
                 SPrimes& sPrimes,
                 IntegerMath& intMath,
-                Mod& mod );
+                Mod& mod ); // ,
+                // Exponents& exp,
+                // FileIO& mainIO );
 
   static bool isPrimeForOneValue(
                       Integer& toTest,
                       Uint64 base,
                       IntegerMath& intMath,
-                      Mod& mod );
+                      Mod& mod ); // ,
+                      // Exponents& exp,
+                      // FileIO& mainIO );
 
 
   };
