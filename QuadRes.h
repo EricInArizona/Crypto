4241292572f3034b1216851999ca12e7c81d0bb3
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-11-17 11:23:17 -0700

    Create QuadRes.h

---
+++ QuadRes.h
@@ -0,0 +1,25 @@
+// Copyright Eric Chauvin, 2021.
+
+
+#pragma once
+
+
+
+#include "BasicTypes.h"
+
+
+
+class QuadRes
+  {
+  private:
+
+  public:
+  static bool bytesQR( const Uint32 test );
+  static bool isSmallQR( const Uint32 number );
+  static bool isQR17( const Uint32 number );
+  static bool isQR19( const Uint32 number );
+  static bool isQR23( const Uint32 number );
+  // static bool isQR29( const Uint32 number );
+
+
+  };
