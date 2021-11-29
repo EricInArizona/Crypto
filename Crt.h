66799ae6f7f3854c68c3165c3e58d05172e8b7df
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-11-29 10:18:31 -0700

    Create Crt.h

---
+++ Crt.h
@@ -0,0 +1,57 @@
+// Copyright Eric Chauvin, 2021.
+
+
+#pragma once
+
+
+
+#include "BasicTypes.h"
+#include "ProjConst.h"
+#include "SPrimes.h"
+
+
+class Crt
+  {
+  private:
+  Int32 testForCopy = 123;
+  static const Uint32 last =
+                 ProjConst::digitArraySize * 2;
+  Int32* digitAr;
+
+  public:
+  Crt( void );
+  Crt( const Crt& in );
+  ~Crt( void );
+  void setToZero();
+  void setToOne();
+  bool isZero();
+  bool isOne();
+  void copy( Crt& toCopy );
+  bool isEqual( Crt& toCheck );
+  void add( Crt& toAdd, SPrimes& sPrimes );
+  void subtract( Crt& toSub, SPrimes& sPrimes );
+  void decrement( SPrimes& sPrimes );
+  void subtractUint( Uint32 toSub,
+                     SPrimes& sPrimes );
+
+
+  inline Int32 getD( Uint32 index )
+    {
+    // Comment out after testing:
+    if( index >= last )
+      throw "Crt GetD Index is too big.";
+
+    return digitAr[index];
+    }
+
+  inline void setD( Int32 setTo, Uint32 index )
+    {
+    if( index >= last )
+      throw "Crt SetD Index is too big.";
+
+    digitAr[index] = setTo;
+    }
+
+
+
+  };
