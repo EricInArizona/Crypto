748ff81c4f9747f498079a0e47fe339b1ddbd3df
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-12-21 11:49:45 -0700

    Update CrtMath.h

--- CrtMath.h
+++ CrtMath.h
@@ -7,6 +7,7 @@
 
 #include "BasicTypes.h"
 #include "Crt.h"
+#include "Crt2.h"
 #include "Integer.h"
 #include "IntegerMath.h"
 #include "IntBuf.h"
@@ -19,6 +20,7 @@ class CrtMath
   private:
   Int32 testForCopy = 123;
   IntBuf* baseAr;
+  Crt* baseArCrt;
   static const Uint32 last =
                    ProjConst::crtDigitArraySize;
 
@@ -31,9 +33,14 @@ class CrtMath
   ~CrtMath( void );
   void init( IntegerMath& intMath,
                              SPrimes& sPrimes );
-  // void getInteger( Integer& accumulate,
-  //                 Crt& input,
-  //                 SPrimes& sPrimes );
+
+  void crt2ToInteger( Crt2& from,
+                      Integer& toSet,
+                      IntegerMath& intMath );
+
+  void crtToCrt2( Crt& from, Crt2& toSet,
+                  SPrimes& sPrimes );
+
 
 
   };
