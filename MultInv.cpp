2d7a86fd1e39fcf28b1cfcfaf02d7e90038f649e
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-07 11:46:25 -0700

    Update MultInv.cpp

--- MultInv.cpp
+++ MultInv.cpp
@@ -20,11 +20,47 @@ u32Arrays = new Uint32Array[last];
 if( in.testForCopy == 7 )
   return;
 
-throw "Don't use copy constructor for MultInv.\n";
+throw "Don't use copy constructor for MultInv.";
 }
 
 
 MultInv::~MultInv( void )
 {
 delete[] u32Arrays;
+}
+
+
+
+Uint32 MultInv::calcMultInv( const Uint32 prime,
+                             const Uint32 test )
+{
+for( Uint32 count = 1; count < prime; count++ )
+  {
+  Uint32 check = test * count;
+  check = check % prime;
+  if( check == 1 )
+    return count;
+
+  }
+
+return 0;
+}
+
+
+
+void MultInv::init( SPrimes& sPrimes )
+{
+for( Uint32 count = 0; count < last; count++ )
+  {
+  Uint32 prime = sPrimes.getPrimeAt( count );
+  u32Arrays[count].setSize( prime );
+  u32Arrays[count].setVal( 0, 0 );
+
+  for( Uint32 countP = 1; countP < prime;
+                                    countP++ )
+    {
+    Uint32 inv = calcMultInv( prime, countP );
+    u32Arrays[count].setVal( countP, inv );
+    }
+  }
 }
