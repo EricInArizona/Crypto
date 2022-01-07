c16188930223b91ff4b3046cd30fe504959e1d2d
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-07 11:47:07 -0700

    Update MultInv.h

--- MultInv.h
+++ MultInv.h
@@ -16,18 +16,24 @@ class MultInv
   {
   private:
   Int32 testForCopy = 123;
-
   static const Uint32 last =
                    ProjConst::crtDigitArraySize;
 
   Uint32Array* u32Arrays;
-
-  // Uint32 prime = sPrimes.getPrimeAt(
-     //      ProjConst::crtDigitArraySize - 1 );
+  Uint32 calcMultInv( const Uint32 prime,
+                             const Uint32 test );
 
   public:
   MultInv( void );
   MultInv( const MultInv& in );
   ~MultInv( void );
+  void init( SPrimes& sPrimes );
+  inline Uint32 getInv( Uint32 primeAt,
+                        Uint32 value )
+    {
+    return u32Arrays[primeAt].getVal( value );
+    }
+
+
 
   };
