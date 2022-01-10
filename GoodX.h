b05a6c484b29a3f877e469829926a30f0193654c
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-10 12:06:13 -0700

    Create GoodX.h

---
+++ GoodX.h
@@ -0,0 +1,45 @@
+// Copyright Eric Chauvin, 2021 - 2022.
+
+
+
+#pragma once
+
+
+#include "BasicTypes.h"
+#include "ProjConst.h"
+#include "SPrimes.h"
+#include "BoolArray.h"
+
+
+
+
+
+class GoodX
+  {
+  private:
+  Int32 testForCopy = 123;
+
+  static const Uint32 last =
+                   ProjConst::crtDigitArraySize;
+
+  BoolArray* bArrays;
+
+  public:
+  GoodX( void );
+  GoodX( const GoodX& in );
+  ~GoodX( void );
+  void init( void );
+  inline bool getVal( Uint32 primeAt,
+                      Uint32 where )
+    {
+    return bArrays[primeAt].getVal( where );
+    }
+
+  inline void setVal( Uint32 primeAt,
+                      Uint32 where,
+                      bool toSet )
+    {
+    return bArrays[primeAt].setVal( where, toSet );
+    }
+
+  };
