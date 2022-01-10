c878b68d289281b511b793223bdaccffbb9faf95
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-10 11:44:46 -0700

    Update QuadRes.h

--- QuadRes.h
+++ QuadRes.h
@@ -1,4 +1,5 @@
-// Copyright Eric Chauvin, 2021.
+// Copyright Eric Chauvin, 2021 - 2022.
+
 
 
 #pragma once
@@ -6,14 +7,28 @@
 
 
 #include "BasicTypes.h"
+#include "ProjConst.h"
+#include "SPrimes.h"
+#include "BoolArray.h"
 
 
 
 class QuadRes
   {
   private:
+  Int32 testForCopy = 123;
+
+  static const Uint32 last =
+                   ProjConst::crtDigitArraySize;
+
+  BoolArray* bArrays;
 
   public:
+  QuadRes( void );
+  QuadRes( const QuadRes& in );
+  ~QuadRes( void );
+  void init( SPrimes& sPrimes );
+
   static bool bytesQR( const Uint32 test );
   static bool isSmallQR( const Uint32 number );
   static bool isQR17( const Uint32 number );
