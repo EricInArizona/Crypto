3906ff12becd652b3af61bfe269eb096ee20e960
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-10 12:16:49 -0700

    Create GoodX.cpp

---
+++ GoodX.cpp
@@ -0,0 +1,48 @@
+// Copyright Eric Chauvin 2022.
+
+
+
+#include "GoodX.h"
+
+
+GoodX::GoodX( void )
+{
+bArrays = new BoolArray[last];
+}
+
+
+GoodX::GoodX( const GoodX& in )
+{
+bArrays = new BoolArray[last];
+
+// Make the compiler think in is being used.
+if( in.testForCopy == 7 )
+  return;
+
+throw "Don't use copy constructor for GoodX.";
+}
+
+
+GoodX::~GoodX( void )
+{
+delete[] bArrays;
+}
+
+
+
+void GoodX::init( SPrimes& sPrimes )
+{
+for( Uint32 count = 0; count < last; count++ )
+  {
+  Uint32 prime = sPrimes.getPrimeAt( count );
+  bArrays[count].setSize( prime );
+
+  for( Uint32 countF = 0; countF < prime;
+                                    countF++ )
+    {
+    bArrays[count].setVal( countF, false );
+    }
+  }
+}
+
+
