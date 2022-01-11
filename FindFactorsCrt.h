812ac4345d02296c609370a75db332a867ae1a79
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-11 11:23:29 -0700

    Create FindFactorsCrt.h

---
+++ FindFactorsCrt.h
@@ -0,0 +1,35 @@
+// Copyright Eric Chauvin, 2022.
+
+
+
+#pragma once
+
+
+
+#include "BasicTypes.h"
+// #include "ProjConst.h"
+#include "SPrimes.h"
+#include "Integer.h"
+#include "IntegerMath.h"
+#include "MultInv.h"
+#include "CrtMath.h"
+
+
+class FindFactorsCrt
+  {
+  private:
+  Int32 testForCopy = 123;
+
+  public:
+  FindFactorsCrt( void );
+  FindFactorsCrt( const FindFactorsCrt& in );
+  ~FindFactorsCrt( void );
+  bool getSmall( Integer& pubKeyN,
+                 Integer& find1,
+                 Integer& find2,
+                 IntegerMath& intMath,
+                 SPrimes& sPrimes,
+                 MultInv& multInv,
+                 CrtMath& crtMath );
+  
+  };
