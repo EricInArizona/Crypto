89256d11d29cc5c054d7b5a77f6fc2d1e151bbab
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-12 12:16:33 -0700

    Update FindFactorsCrt.cpp

--- FindFactorsCrt.cpp
+++ FindFactorsCrt.cpp
@@ -35,7 +35,8 @@ bool FindFactorsCrt::getSmall( Integer& pubKeyN,
                          IntegerMath& intMath,
                          SPrimes& sPrimes,
                          MultInv& multInv,
-                         CrtMath& crtMath )
+                         CrtMath& crtMath,
+                         FileIO& mainIO )
 {
 Crt prod;
 prod.setFromInteger( pubKeyN,
@@ -44,28 +45,63 @@ prod.setFromInteger( pubKeyN,
 
 Crt2 prime1;
 Crt2 prime2;
-Crt crt;
+Crt prime1Crt;
 Crt inv;
+Crt testProd;
 
-for( Uint32 count = 1; count < 5; count++ )
+Integer showIt;
+
+prime1.setToZero();
+
+// See the note in Crt2::setInvCrt() to see
+// why this count goes up above a certain
+// prime number.
+for( Uint32 count = 0; count < 4500; count++ )
   {
-  prime1.setToIndex1( count );
-  prime1.incRev( sPrimes, count );
+  if( !prime1.increment( sPrimes ))
+    break;
 
-  for( Uint32 countI = 0; countI < 5; countI++ )
-    {
-    if( !prime1.incRev( sPrimes, count ))
-      break;
+  if( prime1.getD( 0 ) == 0 )
+    continue;
 
-    if( !prime1.setInvCrt( crt,
-                           inv,
-                           sPrimes,
-                           multInv,
-                           crtMath ))
-      continue;
+  if( !prime1.setInvCrt( prime1Crt,
+                         inv,
+                         prod,
+                         sPrimes,
+                         multInv,
+                         crtMath ))
+    continue;
 
-    }
 
+
+  mainIO.appendChars( "Prime1: " );
+  prime1.toInteger( crtMath,
+                    showIt,
+                    intMath );
+
+
+
+
+  Str showP =  intMath.toString10( showIt );
+  mainIO.appendStr( showP );
+  mainIO.appendChars( "\n" );
+
+  // At this point crt and inv have no small
+  // prime factors in them.
+
+  // There is a big number Big = 2 * 3 * 5 * ...
+  // all the way up to the last prime in a
+  // CRT number.  The value of inv is the
+  // multiplicative inverse mod Big.
+  // It is unlikely to be an actual prime
+  // factor of prod.  But it could be.
+
+  Crt testProd;
+  testProd.copy( prime1Crt );
+  testProd.multiply( inv, sPrimes );
+
+  if( !testProd.isEqual( prod ))
+    throw "!testProd.isEquprime1Crtal( prod )";
 
   }
 
