0b7e486f09f0695bcc480cd4c0100eeb0586ebe3
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2022-01-13 12:08:04 -0700

    Update Uint32Array.h

--- Uint32Array.h
+++ Uint32Array.h
@@ -37,17 +37,17 @@ class Uint32Array
 
   inline Uint32 getVal( const Uint32 where ) const
     {
-    if( where >= arraySize )
-      throw "In getVal() index out of bounds.";
+    // if( where >= arraySize )
+      // throw "In getVal() index out of bounds.";
 
     return uArray[where];
     }
 
-  inline void setVal( const Uint32 where, 
+  inline void setVal( const Uint32 where,
                       const Uint32 toSet )
     {
-    if( where >= arraySize )
-      throw "In setVal() index out of bounds.";
+    // if( where >= arraySize )
+      // throw "In setVal() index out of bounds.";
 
     uArray[where] = toSet;
     }
