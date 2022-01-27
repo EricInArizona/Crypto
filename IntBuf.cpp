d38a1385f5f939e29cdaddc40b0f128f1aaecf4c
Eric Chauvin <echauvin823@gmail.com>
2022-01-27 11:53:29 -0700

    Update IntBuf.cpp

--- IntBuf.cpp
+++ IntBuf.cpp
@@ -1,20 +1,21 @@
-// Copyright Eric Chauvin 2021.
+// Copyright Eric Chauvin 2021 - 2022.
 
 
 
 #include "IntBuf.h"
+#include "CastE.h"
 
 
 IntBuf::IntBuf( void )
 {
-D = new Uint32[last];
+D = new Int32[CastE::i32ToU64( last )];
 }
 
 
 // Copy constructor.
 IntBuf::IntBuf( const IntBuf& in )
 {
-D = new Uint32[last];
+D = new Int32[CastE::i32ToU64( last )];
 
 // Make the compiler think in is being used.
 if( in.testForCopy == 7 )
